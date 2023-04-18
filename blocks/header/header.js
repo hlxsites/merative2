56fade0997cc80de51a49c852c41e47770906b01
Anthony Rumsey <arumsey@adobe.com>
2023-04-18 15:34:36 -0400

    Mega Menu Header #144
    - use primary button
    - add close icon
    - add level 2 title

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -12,7 +12,6 @@ async function fetchFragment(path) {
     const container = document.createElement('div');
     container.innerHTML = await resp.text();
     decorateBlock(container);
-    // decorateMain(container);
     await loadBlocks(container);
     return container;
   }
@@ -25,9 +24,18 @@ async function fetchFragment(path) {
  */
 
 function collapseAllNavSections(sections) {
+  if (!sections) {
+    return;
+  }
   sections.querySelectorAll(':scope > ul > li').forEach((section) => {
     section.setAttribute('aria-expanded', 'false');
   });
+}
+
+function toggleSection(section) {
+  const expanded = section.getAttribute('aria-expanded') === 'true';
+  collapseAllNavSections(section.closest('ul').parentElement);
+  section.setAttribute('aria-expanded', expanded ? 'false' : 'true');
 }
 
 /**
@@ -43,121 +51,141 @@ export default async function decorate(block) {
   // const navPath = cfg.nav || '/nav';
   // const resp = await fetch(`${navPath}.plain.html`);
   const resp = await fetch('/drafts/amol/nav.plain.html');
-  if (resp.ok) {
-    const html = await resp.text();
+  if (!resp.ok) {
+    return;
+  }
 
-    // decorate nav DOM
-    const nav = document.createElement('nav');
-    nav.innerHTML = html;
-    decorateIcons(nav);
+  const html = await resp.text();
 
-    const classes = ['brand', 'sections', 'tools'];
-    classes.forEach((e, j) => {
-      const section = nav.children[j];
-      if (section) {
-        section.classList.add(`nav-${e}`);
-        if (e === 'tools') {
-          decorateButtons(section);
-        }
+  // decorate nav DOM
+  const nav = document.createElement('nav');
+  nav.innerHTML = html;
+  decorateIcons(nav);
+
+  const navChildren = [...nav.children];
+  const classes = ['brand', 'sections', 'tools'];
+
+  navChildren.forEach((section, index) => {
+    const sectionName = classes[index];
+    section.classList.add(`nav-${sectionName}`);
+    if (sectionName === 'tools') {
+      decorateButtons(section);
+    }
+  });
+
+  const navSections = navChildren[1];
+  if (navSections) {
+    navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
+      // deal with top level dropdowns first
+      if (navSection.querySelector('ul')) {
+        navSection.classList.add('nav-drop');
       }
-    });
-
-    const navSections = [...nav.children][1];
-    if (navSections) {
-      navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
-        // deal with top level dropdowns first
-        if (navSection.querySelector('ul')) {
-          navSection.classList.add('nav-drop');
+      // replacing bold nav titles with divs for styling
+      if (navSection.querySelector('strong')) {
+        const sectionHeading = navSection.querySelector('strong');
+        const sectionHeadingNew = document.createElement('div');
+        sectionHeadingNew.classList.add('section-heading');
+        sectionHeadingNew.textContent = sectionHeading.textContent;
+        navSection.replaceChild(sectionHeadingNew, sectionHeading);
+      }
+      navSection.addEventListener('click', () => {
+        toggleSection(navSection);
+      });
+      // Setup level 2 links
+      navSection.querySelectorAll(':scope > ul > li').forEach((levelTwo) => {
+        const megaTitle = levelTwo.querySelector(':scope > em');
+        if (megaTitle) {
+          const megaTitleNew = document.createElement('h3');
+          megaTitleNew.innerText = megaTitle.innerText;
+          levelTwo.replaceWith(megaTitleNew);
+          return;
         }
-        // replacing bold nav titles with divs for styling
-        if (navSection.querySelector('strong')) {
-          const sectionHeading = navSection.querySelector('strong');
-          const sectionHeadingNew = document.createElement('div');
-          sectionHeadingNew.classList.add('section-heading');
-          sectionHeadingNew.textContent = sectionHeading.textContent;
-          navSection.replaceChild(sectionHeadingNew, sectionHeading);
+        const megaHeading = levelTwo.querySelector(':scope > strong');
+        if (megaHeading) {
+          // mega menu
+          const megaHeadingNew = document.createElement('div');
+          megaHeadingNew.classList.add('level-two-heading');
+          megaHeadingNew.innerText = megaHeading.innerText;
+          megaHeading.remove();
+          levelTwo.prepend(megaHeadingNew);
+          levelTwo.classList.add('mega-menu');
+          levelTwo.parentElement.classList.add('mega-menu');
         }
-        navSection.addEventListener('click', () => {
-          const expanded = navSection.getAttribute('aria-expanded') === 'true';
-          collapseAllNavSections(navSections);
-          navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+        levelTwo.classList.add('level-two');
+        levelTwo.parentElement.classList.add('level-two');
+        levelTwo.addEventListener('click', (event) => {
+          toggleSection(levelTwo);
+          event.stopPropagation();
         });
-        // Setup level 2 links
-        navSection.querySelectorAll(':scope > ul > li').forEach((levelTwo) => {
-          const l2Heading = levelTwo.querySelector(':scope > strong');
-          if (l2Heading) {
-            const l2HeadingNew = document.createElement('div');
-            l2HeadingNew.classList.add('level-two-heading');
-            l2HeadingNew.innerText = l2Heading.innerText;
-            l2Heading.remove();
-            levelTwo.prepend(l2HeadingNew);
-          } else {
-            // this is a level 2 link that has the old look and feel
-            levelTwo.classList.add('old');
-            levelTwo.parentElement.classList.add('old');
-          }
-          levelTwo.classList.add('level-two');
-          levelTwo.parentElement.classList.add('level-two');
-          levelTwo.addEventListener('click', (event) => {
-            const expanded = levelTwo.getAttribute('aria-expanded') === 'true';
-            collapseAllNavSections(navSection);
-            levelTwo.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+        // Setup level 3 links
+        levelTwo.querySelectorAll(':scope > ul > li').forEach((levelThree) => {
+          levelThree.classList.add('level-three');
+          levelThree.addEventListener('click', (event) => {
+            const expanded = levelThree.getAttribute('aria-expanded') === 'true';
+            collapseAllNavSections(levelTwo);
+            levelThree.setAttribute('aria-expanded', expanded ? 'false' : 'true');
             event.stopPropagation();
           });
-          // Setup level 3 links
-          levelTwo.querySelectorAll(':scope > ul > li').forEach((levelThree) => {
-            levelThree.classList.add('level-three');
-            levelThree.addEventListener('click', (event) => {
-              const expanded = levelThree.getAttribute('aria-expanded') === 'true';
-              collapseAllNavSections(levelTwo);
-              levelThree.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-              event.stopPropagation();
-            });
-            // Setup level 4 links
-            levelThree.querySelectorAll(':scope > ul > li').forEach((levelFour) => {
-              levelFour.classList.add('level-four');
-            });
+          // Setup level 4 links
+          levelThree.querySelectorAll(':scope > ul > li').forEach((levelFour) => {
+            levelFour.classList.add('level-four');
           });
         });
       });
-    }
 
-    // Auto block fragment urls
-    await Promise.all([...nav.querySelectorAll('a')].map(async (link) => {
-      const url = new URL(link.href);
-      if (url.pathname.startsWith('/fragments/')) {
-        const fragmentBlock = await fetchFragment(link.href);
-        link.parentElement.append(fragmentBlock);
-        link.remove();
-        // link.append(fragmentBlock);
-        return true;
-      }
-      return null;
-    }));
-
-    // add page scroll listener to know when header turns to sticky
-    const header = block.parentNode;
-    window.addEventListener('scroll', () => {
-      const scrollAmount = window.scrollY;
-      if (scrollAmount > header.offsetHeight) {
-        header.classList.add('is-sticky');
-      } else {
-        header.classList.remove('is-sticky');
+      const sectionMenu = navSection.querySelector('ul.mega-menu');
+      if (sectionMenu) {
+        // add close icon
+        const closeLink = document.createElement('a');
+        closeLink.className = 'close';
+        closeLink.setAttribute('aria-label', 'Close');
+        closeLink.innerHTML = '<span class="icon icon-x" />';
+        sectionMenu.appendChild(closeLink);
+        closeLink.addEventListener('click', (event) => {
+          toggleSection(navSection);
+          event.stopPropagation();
+        })
       }
     });
-
-    // hamburger for mobile
-    const hamburger = document.createElement('div');
-    hamburger.classList.add('nav-hamburger');
-    hamburger.innerHTML = '<div class="nav-hamburger-icon"></div>';
-    hamburger.addEventListener('click', () => {
-      const expanded = nav.getAttribute('aria-expanded') === 'true';
-      document.body.style.overflowY = expanded ? '' : 'hidden';
-      nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-    });
-    nav.append(hamburger);
-    nav.setAttribute('aria-expanded', 'false');
-    decorateIcons(nav);
-    block.append(nav);
   }
+
+  // Auto block fragment urls
+  await Promise.all([...nav.querySelectorAll('a')].map(async (link) => {
+    if (!link.href) {
+      return null;
+    }
+    const url = new URL(link.href);
+    if (url.pathname.startsWith('/fragments/')) {
+      const fragmentBlock = await fetchFragment(link.href);
+      link.parentElement.append(fragmentBlock);
+      link.remove();
+      return true;
+    }
+    return null;
+  }));
+
+  // add page scroll listener to know when header turns to sticky
+  const header = block.parentNode;
+  window.addEventListener('scroll', () => {
+    const scrollAmount = window.scrollY;
+    if (scrollAmount > header.offsetHeight) {
+      header.classList.add('is-sticky');
+    } else {
+      header.classList.remove('is-sticky');
+    }
+  });
+
+  // hamburger for mobile
+  const hamburger = document.createElement('div');
+  hamburger.classList.add('nav-hamburger');
+  hamburger.innerHTML = '<div class="nav-hamburger-icon"></div>';
+  hamburger.addEventListener('click', () => {
+    const expanded = nav.getAttribute('aria-expanded') === 'true';
+    document.body.style.overflowY = expanded ? '' : 'hidden';
+    nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+  });
+  nav.append(hamburger);
+  nav.setAttribute('aria-expanded', 'false');
+  decorateIcons(nav);
+  block.append(nav);
 }
