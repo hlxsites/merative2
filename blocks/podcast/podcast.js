6df302fb661a3d6fb94e1665a61253175097d0f3
unknown <nshetty@merative.com>
2023-06-16 12:46:59 +0530

    Podcast block creation.

---
+++ blocks/podcast/podcast.js
@@ -0,0 +1,12 @@
+import { createTag, getMetadata } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  
+  const podcastContainer = createTag('div', { class: 'podcast-container' });
+  const podcastIframe = createTag('iframe', { class: 'podcast-iframe' });
+  const podcastIframeUrl = block.querySelector('a').href;
+  podcastIframe.setAttribute('src', podcastIframeUrl);
+  podcastIframe.setAttribute('scrolling', 'no');
+  podcastContainer.append(podcastIframe);
+  block.append(podcastContainer);
+}
