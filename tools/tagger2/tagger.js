4266492824549cd1f41201ebca06211cf3f3528b
chelms <charity@adobe.com>
2023-06-22 16:42:27 -0400

    fix: linting

--- tools/tagger2/tagger.js
+++ tools/tagger2/tagger.js
@@ -4,7 +4,7 @@ function renderItems(items, catId) {
     const { title, path } = tag;
     html += `
       <span class="path">${path}
-        <span data-title="${title}" class="tag cat-${catId % 4}">${title}</span>
+        <span data-title="${title}" class="tag cat-${catId % 8}">${title}</span>
       </span>
     `;
     html += renderItems(tag.children, catId);
