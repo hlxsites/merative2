c8b916463b0e155495c272c6110cc8123207cb93
unknown <nshetty@merative.com>
2023-07-05 11:12:14 +0530

    Es lint corrections for the js file.

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,19 +1,18 @@
-import { createTag } from '../../scripts/scripts.js';
 import { buildBlock } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
-    const h1 = block.querySelector('h1');
-    const picture = block.querySelector('picture');
-    const icon = block.querySelector('span');
-    picture.classList.add('main-banner');
-    icon.setAttribute('id', 'banner-icon');
-      const section = document.createElement('div');
-      const sectionTwo = document.createElement('div');
-      sectionTwo.append(icon);
-      sectionTwo.append(h1);
-      sectionTwo.setAttribute('id', 'logo-text-holder');
-      section.append(buildBlock('hero', { elems: [picture, sectionTwo] }));    
-      const pictureParent = section.querySelector('picture').parentNode;
-      pictureParent.setAttribute('class', 'picture-parent');
-      block.prepend(section);
+  const h1 = block.querySelector('h1');
+  const picture = block.querySelector('picture');
+  const icon = block.querySelector('span');
+  picture.classList.add('main-banner');
+  icon.setAttribute('id', 'banner-icon');
+  const section = document.createElement('div');
+  const sectionTwo = document.createElement('div');
+  sectionTwo.append(icon);
+  sectionTwo.append(h1);
+  sectionTwo.setAttribute('id', 'logo-text-holder');
+  section.append(buildBlock('hero', { elems: [picture, sectionTwo] }));
+  const pictureParent = section.querySelector('picture').parentNode;
+  pictureParent.setAttribute('class', 'picture-parent');
+  block.prepend(section);
 }
