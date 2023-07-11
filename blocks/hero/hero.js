8d8403d5c51db4f120024f34717897cb2a7925d5
unknown <nshetty@merative.com>
2023-07-11 13:52:43 +0530

    feat(hero-banner): Code refactoring changes.

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -4,14 +4,14 @@ export default function decorate(block) {
   const h1 = block.querySelector('h1');
   const icon = block.querySelector('span');
   const allPictures = block.querySelectorAll('.only-picture picture img');
+  const imageAltText = h1.innerHTML || 'hero-preview-image';
 
   const resolutions = ['1200px', '768px', '375px'];
-
   icon.setAttribute('id', 'banner-icon');
   const sectionTwo = document.createElement('div');
   sectionTwo.append(icon);
   sectionTwo.append(h1);
-  sectionTwo.setAttribute('class', 'logo-text-holder');
+  sectionTwo.setAttribute('class', 'hero__content');
 
   // create the <picture> element
   const onePictureElement = document.createElement('picture');
@@ -19,7 +19,7 @@ export default function decorate(block) {
   const imgElement = document.createElement('img');
   imgElement.setAttribute('src', 'path/to/image.jpg');
   imgElement.setAttribute('id', 'imgcls');
-  imgElement.setAttribute('alt', 'Image description');
+  imgElement.setAttribute('alt', imageAltText);
   // Loop through the each <picture> element
   allPictures.forEach((pictureElement, index) => {
     const sourceElement = document.createElement('source');
