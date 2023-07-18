44a04cff742e6656bd7693b9d0d6d9ad6d4085e2
chelms <charity@adobe.com>
2023-07-18 16:41:15 -0400

    add hlx.page to CSP

--- tools/sidekick/plugins/icons/icons.js
+++ tools/sidekick/plugins/icons/icons.js
@@ -9,7 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-import {createElement} from '../../library-utils.js';
+import { createElement } from '../../library-utils.js';
 
 async function processIcons(pageBlock, path) {
   const icons = {};
