51b509a232f74f77b4988851264574b3b3bd8df2
unknown <nshetty@merative.com>
2023-08-24 17:54:00 +0530

    feature(case-study-carousel): Case study carousel block.

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -42,22 +42,22 @@ let maxSlide = 0;
 let carouselType = 'default';
 
 /**
- * Clear any active scroll intervals
- */
+  * Clear any active scroll intervals
+  */
 function stopAutoScroll() {
   clearInterval(scrollInterval);
   scrollInterval = undefined;
 }
 
 /**
- * Count how many lines a block of text will consume when wrapped within a container
- * that has a maximum width.
- * @param text The full text
- * @param width Width of container
- * @param options Options to be applied to context (e.g. font style)
- *
- * @return {number} The number of lines
- */
+  * Count how many lines a block of text will consume when wrapped within a container
+  * that has a maximum width.
+  * @param text The full text
+  * @param width Width of container
+  * @param options Options to be applied to context (e.g. font style)
+  *
+  * @return {number} The number of lines
+  */
 function getLineCount(text, width, options = {}) {
   // re-use canvas object for better performance
   const canvas = getLineCount.canvas || (getLineCount.canvas = document.createElement('canvas'));
@@ -82,11 +82,11 @@ function getLineCount(text, width, options = {}) {
 }
 
 /**
- * Calculate the actual height of a slide based on its contents.
- *
- * @param carousel The carousel
- * @param slide A slide within the carousel
- */
+  * Calculate the actual height of a slide based on its contents.
+  *
+  * @param carousel The carousel
+  * @param slide A slide within the carousel
+  */
 function calculateSlideHeight(carousel, slide) {
   if (carouselType === 'default' || carouselType === 'testimonial') {
     requestAnimationFrame(() => {
@@ -110,10 +110,10 @@ function calculateSlideHeight(carousel, slide) {
 }
 
 /**
- * Keep active dot in sync with current slide
- * @param carousel The carousel
- * @param activeSlide {number} The active slide
- */
+  * Keep active dot in sync with current slide
+  * @param carousel The carousel
+  * @param activeSlide {number} The active slide
+  */
 function syncActiveDot(carousel, activeSlide) {
   carousel.querySelectorAll('ul.carousel-dots li').forEach((item, index) => {
     const btn = item.querySelector('button');
@@ -130,11 +130,11 @@ function syncActiveDot(carousel, activeSlide) {
 }
 
 /**
- * Scroll a single slide into view.
- *
- * @param carousel The carousel
- * @param slideIndex {number} The slide index
- */
+  * Scroll a single slide into view.
+  *
+  * @param carousel The carousel
+  * @param slideIndex {number} The slide index
+  */
 function scrollToSlide(carousel, slideIndex = 0) {
   const carouselSlider = carousel.querySelector('.carousel-slide-container');
   calculateSlideHeight(carouselSlider, carouselSlider.children[slideIndex]);
@@ -154,13 +154,13 @@ function scrollToSlide(carousel, slideIndex = 0) {
 }
 
 /**
- * Based on the direction of a scroll snap the scroll position based on the
- * offset width of the scrollable element. The snap threshold is determined
- * by the direction of the scroll to ensure that snap direction is natural.
- *
- * @param el the scrollable element
- * @param dir the direction of the scroll
- */
+  * Based on the direction of a scroll snap the scroll position based on the
+  * offset width of the scrollable element. The snap threshold is determined
+  * by the direction of the scroll to ensure that snap direction is natural.
+  *
+  * @param el the scrollable element
+  * @param dir the direction of the scroll
+  */
 function snapScroll(el, dir = 1) {
   if (!el) {
     return;
@@ -179,11 +179,11 @@ function snapScroll(el, dir = 1) {
 }
 
 /**
- * Build a navigation button for controlling the direction of carousel slides.
- *
- * @param dir A string of either 'prev or 'next'
- * @return {HTMLDivElement} The resulting nav element
- */
+  * Build a navigation button for controlling the direction of carousel slides.
+  *
+  * @param dir A string of either 'prev or 'next'
+  * @return {HTMLDivElement} The resulting nav element
+  */
 function buildNav(dir) {
   const btn = document.createElement('div');
   btn.classList.add('carousel-nav', `carousel-nav-${dir}`);
@@ -202,10 +202,10 @@ function buildNav(dir) {
 }
 
 /**
- *
- * @param slides An array of slide elements within the carousel
- * @return {HTMLUListElement} The carousel dots element
- */
+  *
+  * @param slides An array of slide elements within the carousel
+  * @return {HTMLUListElement} The carousel dots element
+  */
 function buildDots(slides = []) {
   const dots = document.createElement('ul');
   dots.classList.add('carousel-dots');
@@ -243,12 +243,12 @@ function buildDots(slides = []) {
 }
 
 /**
- * Decorate a base slide element.
- *
- * @param slide A base block slide element
- * @param index The slide's position
- * @return {HTMLUListElement} A decorated carousel slide element
- */
+  * Decorate a base slide element.
+  *
+  * @param slide A base block slide element
+  * @param index The slide's position
+  * @return {HTMLUListElement} A decorated carousel slide element
+  */
 function buildSlide(slide, index) {
   slide.setAttribute('id', `${SLIDE_ID_PREFIX}${index}`);
   slide.setAttribute('data-slide-index', index);
@@ -289,18 +289,21 @@ function startAutoScroll(block) {
 }
 
 /**
- * Decorate and transform a carousel block.
- *
- * @param block HTML block from Franklin
- */
+  * Decorate and transform a carousel block.
+  *
+  * @param block HTML block from Franklin
+  */
 export default function decorate(block) {
   const carousel = document.createElement('div');
   carousel.classList.add('carousel-slide-container');
   if (block.classList.contains('image-carousel-full-width')) {
     carouselType = 'image-carousel-full-width';
-  }
-  if (block.classList.contains('testimonial')) {
+  } else if (block.classList.contains('testimonial')) {
     carouselType = 'testimonial';
+  } else if (block.classList.contains('case-study')) {
+    carouselType = 'case-study';
+  } else {
+    carouselType = 'default';
   }
 
   // make carousel draggable
@@ -349,6 +352,19 @@ export default function decorate(block) {
   const slides = [...block.children];
   maxSlide = slides.length - 1;
   slides.forEach((slide, index) => {
+    if (carouselType === 'case-study') {
+      const titleTag = slide.querySelector('p');
+      const contentDivElement = titleTag.parentElement;
+      const parentDivElement = contentDivElement.parentElement;
+      const titleDiv = document.createElement('div');
+      const contentDiv = document.createElement('div');
+      contentDiv.classList.add('case-study__container-wrapper');
+      titleDiv.classList.add('case-study__title-wrapper');
+      contentDivElement.classList.add('case-study__content-wrapper');
+      titleDiv.append(titleTag);
+      contentDiv.append(titleDiv, contentDivElement);
+      parentDivElement.prepend(contentDiv);
+    }
     carousel.appendChild(buildSlide(slide, index));
   });
 
@@ -356,7 +372,9 @@ export default function decorate(block) {
   block.append(carousel);
 
   // calculate height of first slide
-  calculateSlideHeight(carousel, slides[0]);
+  if (carouselType !== 'case-study') {
+    calculateSlideHeight(carousel, slides[0]);
+  }
 
   // add nav buttons and dots to block
   if (slides.length > 1) {
